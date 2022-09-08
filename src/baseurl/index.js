d9ff04b3fc7417a33fab4bcb01482a6be085be0c
hilalahmad32 <programmerhero6@gmail.com>
2022-09-08 19:18:00 +0500

    add some file

--- src/baseurl/index.js
+++ src/baseurl/index.js
@@ -1,5 +1,5 @@
 // live url
-// export const BASE_URL = "https://coinbid11.herokuapp.com/";
-export const BASE_URL = "http://localhost:5000/";
+ export const BASE_URL = "https://coinbid11.herokuapp.com/";
+//export const BASE_URL = "http://localhost:5000/";
 // local url
 export const LOCAL_BASE_URL = "";
